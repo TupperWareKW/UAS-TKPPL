932912210457246ea77f5fae7f9fe8e52de9f9ed
Fikry Aulya <fikryaulya14@gmail.com>
2023-02-02 23:07:29 +0700

    Add Function Halaman-Keranjang
    Menambahkan fungsionalitas pada halaman keranjang

--- pages/chart.js
+++ pages/chart.js
@@ -40,6 +40,75 @@ const Chart = {
     },
 
     async afterRender() {
+        const products = InfoUser.chart;
+        // console.log(products);
+
+        if(products.length > 1) {
+            const containerCard = document.querySelector('.card-container');
+            containerCard.innerHTML = '';
+            products.forEach((el,i) => {
+                if(el !== 'empty') {
+                    // console.log(products[i][0])
+                    containerCard.innerHTML += `
+                    <div class="card-product">
+                        <div class="check-box">
+                            <input type="checkbox" name="" id="" class="check-box-product">
+                        </div>
+                        <div class="chart-info-product">
+                            <div class="box-img">
+                                <img src="${products[i][2]}">
+                            </div>
+                            <div class="box-info-product">
+                                <p class="chart-name-product">${products[i][0]}</p>
+                                <p class="chart-price-product">${products[i][1]}</p>
+                                <button class="delete-product">Hapus</button>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                }
+                
+            })            
+            
+        }else {
+            const containerCard = document.querySelector('.card-container');
+            containerCard.innerHTML = `<h4>Keranjang belanja masih kosong</h4>`
+        }
+
+        // delete
+        const buttonDelete = document.querySelectorAll('.delete-product');
+        buttonDelete.forEach((el, i) => {
+            el.onclick = event => {
+                const test = InfoUser.chart.filter(el => {
+                    if(el !== 'empty') {
+                        // console.log(el[0] == event.target.parentElement.querySelector('.chart-name-product').innerText) 
+                        return el[0] !== event.target.parentElement.querySelector('.chart-name-product').innerText;
+                    }                    
+                });
+                InfoUser.chart = ['empty'];
+                test.forEach(el => InfoUser.chart.push(el));
+                saveToFirebase(InfoUser);
+                this.afterRender();
+            }
+        });
+
+        // checkbox 
+        const checkBox = document.querySelectorAll('.check-box-product');
+        let totalPriceProduct = 0;
+        checkBox.forEach(el => {
+            el.onclick = (event) => {
+                const priceProduct = event.target.parentElement.parentElement.querySelector('.chart-price-product').innerText.substr(3,5);
+                const spanPriceProduct = document.querySelector('#total-harga-product');
+                if(event.target.checked === true) {                    
+                    totalPriceProduct += +priceProduct;
+                    spanPriceProduct.innerText = totalPriceProduct;
+                }else {
+                    totalPriceProduct -= +priceProduct;
+                    spanPriceProduct.innerText = totalPriceProduct;
+                }
+            }
+        })
+
     }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
+import Chart from "../pages/chart.js";
 import Home from "../pages/home.js";
 import Landing from "../pages/landing.js";
 import Profile from "../pages/profile.js";
