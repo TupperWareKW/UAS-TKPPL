51a1de213d228b2267cfa2f8f0ff8eadd36955be
Arif Ambarita <ambaritamuhammadarif@gmail.com>
2023-02-02 14:17:21 +0700

    Add Halaman-Beranda

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
+import Home from "../pages/home.js";
 import Landing from "../pages/landing.js";
 
 const routes = {
     '': Landing,
+    'home': Home,
 }
 
 export default routes;
---
+++ scripts/data/data-produck.js
@@ -0,0 +1,31 @@
+const dataImg = [
+    'https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/12/7/517dd9a6-b16d-42df-8427-0bf712325569.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/8/4/3379a3f8-bc02-4f09-9c28-acd9e00e0313.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/2/9/aecb4e2c-964a-48cf-8807-824a82c7dfd2.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/hDjmkQ/2021/11/10/f32d62d3-63e4-4da5-a06e-e0b333a280b4.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/hDjmkQ/2022/9/28/dfac8a35-9be6-4142-832a-2ac84c13f48f.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/4/2/3232fb52-3349-4988-ba32-10c87d893be9.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/hDjmkQ/2022/1/10/9da35bbc-559e-4c3f-9a6e-c476398fc413.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/6/12/8e78f2d1-628c-475a-a056-3ea53e83748f.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/hDjmkQ/2021/10/15/82a0dfbb-2687-428e-af18-36a4495e1c4e.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/hDjmkQ/2021/11/12/b4c75690-edb2-4bd9-8329-7aa9e95691e2.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/6/13/adf9ee26-c4f5-4dbc-b25b-c70d767d73ab.jpg',
+    'https://images.tokopedia.net/img/cache/200-square/hDjmkQ/2021/11/10/18f57553-ca76-4c66-b36b-b68e17b0c592.jpg'
+]
+
+const nameProduck = [
+    'Tupperware Midi Canister',
+    'Square Round Tupperware',
+    'Tupperware CozyNest Small',
+    'Tupperware Giant Canister',
+    'Tupperware Vivid Bowl',
+    'Tupperware Square 4 ungu',
+    'Tupperware Tall Canister Giant',
+    'Tupperware Seal Giant Canister',
+    'Tupperware Giant Canister 5ltr',
+    'Tupperware Cozy Nest Large',
+    'Tupperware Bowl Small 200ml',
+    'Tupperware Disney Canister'
+]
+
+export {nameProduck, dataImg};
